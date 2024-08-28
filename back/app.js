6ea39cba5827f6822f734f5b8f672b3e92b46c3e
HP <31686938+yougo96@users.noreply.github.com>
2024-08-28 12:11:27 +0200

    setup mongo

--- back/app.js
+++ back/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://admin:admin@clustermvg.92nqh.mongodb.net/test?retryWrites=true&w=majority',
+    { useNewUrlParser: true,
+      useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => {
   console.log('Requête reçue !', req.url);
